859bfdc2bac1267eccfa978b4c4c43676fcd2391
Brien Thomas <thomasbrien94@gmail.com>
2022-08-02 15:13:48 -0700

    removed my irritated comments

--- exercise_9/exercise-9.sql
+++ exercise_9/exercise-9.sql
@@ -2,10 +2,6 @@
 -- LEFT JOIN FROM inventory to film and to rental
 -- use WHERE IS NULL
 
--- This doesn't make any sense, why are we looking for the inventory_id to be
--- null? If it hasn't been rented, it would be in the inventory, and we
--- should be looking at the rental_date to be null.
-
 SELECT title 
 FROM film
 INNER JOIN inventory ON film.film_id = inventory.film_id
